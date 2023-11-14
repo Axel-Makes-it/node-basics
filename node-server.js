52b6408eaf517e6c6c978dba874f93e093b36848
Axel Ivan Santiago <axelivanlux@gmail.com>
2023-11-14 11:50:39 -0500

    update

--- node-server.js
+++ node-server.js
@@ -1,7 +1,17 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("Node Server!");
+  // Routes
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<h1>Home page...</h1>");
+    res.end();
+  }
+  if (req.url === "/about") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<h2>About page...</h2>");
+    res.end();
+  }
 });
 
 server.listen("5000", "localhost", () => {
