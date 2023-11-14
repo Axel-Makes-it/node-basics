223391754819f8caf136277392e662cbc1e9f462
Axel Ivan Santiago <axelivanlux@gmail.com>
2023-11-14 13:16:43 -0500

    update

---
+++ express-server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("<h2>Welcome to the home page!</h2>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h3>About page!</h3>");
+});
+
+app.listen("5000", () => {
+  console.log("Server runing on port 5000");
+});
