dc87139371e6af1c9cf7346cdde32e5de9f53585
Axel Ivan Santiago <axelivanlux@gmail.com>
2023-11-14 10:55:43 -0500

    update

--- node-server.js
+++ node-server.js
@@ -1,3 +1,12 @@
 const http = require("http");
 
-http.createServer(() => {});
+const server = http.createServer((req, res) => {
+  console.log("Node Server!");
+});
+
+server.listen("5000", "localhost", () => {
+  console.log("Server running on port 5000");
+});
+
+// 127.0.0.1 = localhost
+// port 5000
