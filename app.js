9241b07f5452d3fbd96901f2465f061ff5ff45e0
Axel Ivan Santiago <axelivanlux@gmail.com>
2023-11-14 10:45:50 -0500

    update

---
+++ app.js
@@ -0,0 +1,22 @@
+const { person, people } = require("./person");
+
+console.log(person);
+console.log(people);
+
+// CommonJS
+//ES module
+
+//-------------------------------
+// const greet = (name) => {
+//   console.log(name);
+// };
+
+// greet("Axel");
+// greet("John");
+
+// globalThis.setInterval(() => {
+//   console.log("Mern Stack Courses");
+// }, 3000);
+
+// console.log(__dirname);
+// console.log(__filename);
---
+++ person.js
@@ -0,0 +1,9 @@
+const person = "Zino";
+const people = ["Zino", "John", "Komen"];
+
+// module.exports = person;
+
+module.exports = {
+  person,
+  people,
+};
